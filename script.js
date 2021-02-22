15969e248abd288fc5d6dd7142a9b97d1a7de229
Van <vango85@gmail.com>
2021-02-22 12:09:27 +0100

    [xx] Created list of recipes from API and added them to HTML

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => {
+    const posts = json;
+    posts.forEach((post, index) => {
+      const newRecipeElement = `<section class="recipe">Recipe #${index} : ${post.title}</section>`;
+      document.getElementById('recipeList').innerHTML += newRecipeElement;
+    });
+  });
